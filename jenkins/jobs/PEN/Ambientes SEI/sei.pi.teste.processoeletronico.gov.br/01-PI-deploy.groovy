2e2f845c4602ebf8375ef7b17eab1615695f92a3
Marcelo Linhares Castro <marlinhares@gmail.com>
2026-06-16 18:17:05 -0300

    Reorganizando para migracao Huawei

---
+++ jenkins/jobs/PEN/Ambientes SEI/sei.pi.teste.processoeletronico.gov.br/01-PI-deploy.groovy
@@ -0,0 +1,381 @@
+/*
+
+Job para Publicar o SEI em Cluster Kubernetes
+
+*/
+
+pipeline {
+    agent {
+        node{
+            label "SEI-FONTE"
+        }
+    }
+
+    parameters {
+
+        booleanParam(
+            name: 'Leiame',
+            defaultValue: false,
+            description: 'Atenção. A versão dos módulos ou do SEI pode ser o hash do commit; tag; branch; Antes de selecionar uma versão para os módulos verifique se o conteiner app-ci está buildado em uma data posterior ao commit que vc escolheu, caso contrário vai dar erro ao subir o ambiente. Em caso de necessidade de buildar o app-ci, caso vc não seja o dono do registry acione os donos para buildar os conteineres usando o projeto sei-docker')
+        string(
+	        name: 'versaoSei',
+	        defaultValue:"4.0.12",
+	        description: "Versao para o SEI, pode ser uma branch, tag ou commit. Exemplo: master, 4.0.12, v4.0.12, etc")
+	    choice(
+            name: 'moduloPIInstalar',
+            choices: ['true', 'false'],
+            description: 'Instalar Módulo Protocolo Integrado')
+        string(
+            name: 'moduloPIVersao',
+            defaultValue:"master",
+            description: "Versao do Módulo Protocolo Integrado. Vai apontar para o PI de homolog. Nome Orgao: Teste 5.0")
+        string(
+            name: 'moduloPIEmail',
+            defaultValue:"example@example.com",
+            description: "Email do Módulo do Protocolo Integrado")
+
+    }
+
+    stages {
+
+        stage('Inicializar Job'){
+            steps {
+
+                script{
+
+                    env.JOB_URL = "sei.pi.teste.processoeletronico.gov.br"
+                    env.JOB_ORGAO = "PI"
+                    env.JOB_NS = "sei-pi"
+
+                    env.JOB_MODULOPI_URL = "https://protocolointegrado.preprod.nuvem.gov.br/ProtocoloWS/integradorService?wsdl"
+                    env.JOB_MODULOPI_USUARIO = "credModuloPIUsuaro"
+                    env.JOB_MODULOPI_SENHA = "credModuloPISenha"
+
+                    env.GITSEIPAT = "github_pat_readonly_pengovbr"
+                    env.GITMODULOPAT = "github_pat_readonly_pengovbr"
+                    env.GITSEIDOCKERURL = "https://github.com/pengovbr/sei-docker.git"
+                    env.GITSEIURL = "https://github.com/pengovbr/sei.git"
+
+                    env.GITSEIVERSAO = params.versaoSei
+
+                    env.MODULOPI_INSTALAR = params.moduloPIInstalar
+                    env.MODULOPI_VERSAO = params.moduloPIVersao
+                    env.MODULOPI_EMAIL = params.moduloPIemail
+                    env.MODULOPI_URL = JOB_MODULOPI_URL
+                    env.MODULOPI_USUARIO = JOB_MODULOPI_USUARIO
+                    env.MODULOPI_SENHA = JOB_MODULOPI_SENHA
+
+                    if ( env.BUILD_NUMBER == '1' ){
+                        currentBuild.result = 'ABORTED'
+                        warning('Informe os valores de parametro iniciais. Caso eles n tenham aparecido faça login novamente')
+                    }
+
+                }
+
+            }
+        }
+
+        stage('Baixar SEI.php'){
+
+            steps {
+
+                dir('sei'){
+
+                    sh """
+                    rm -rf *
+                    git config --global http.sslVerify false
+                    """
+
+                    git branch: 'main',
+                        url: GITSEIURL,
+                        credentialsId: GITSEIPAT
+
+                    sh """
+                    ls -l
+
+                    git checkout ${GITSEIVERSAO}
+
+
+                    """
+
+                }
+            }
+        }
+
+        stage('Checkout-ProjetoKube'){
+
+            steps {
+
+                dir('kube'){
+
+                    sh """
+                    git config --global http.sslVerify false
+                    """
+
+                    git branch: 'main',
+                        url: GITSEIDOCKERURL
+
+                    sh """
+                    ls -l
+                    """
+
+                }
+            }
+        }
+
+        stage('Deletar e Subir Projeto Kubernetes'){
+
+            steps {
+
+                sh """
+                pwd
+                ls -lh
+
+                d=sei/
+                if [ -d sei/src ]; then d=sei/src; fi
+                    cd \$d
+                    set +e
+                    grep -e "const SEI_VERSAO = '5\\..*\\..*';" sei/web/SEI.php
+                    e=\$?
+                    set -e
+                    if [ -d ../src ]; then cd ..; fi
+                    cd ../kube
+
+
+                    if [ "\$e" = "0" ]; then
+
+                        cat infra/envlocal-example-mysql-sei5.env >> infra/envlocal.env
+
+                    fi
+
+
+                """
+
+                dir('kube'){
+
+                    withCredentials([usernamePassword(credentialsId: GITSEIPAT, usernameVariable: 'USER', passwordVariable: 'LHAVE')]) {
+
+                        sh """
+                        cd infra
+                        echo "" >> envlocal.env
+                        echo "export APP_FONTES_GITHUB_TOKEN=${LHAVE}" >> envlocal.env
+                        """
+
+                        sh """
+
+                        cd infra
+                        echo "" >> envlocal.env
+                        echo "export GITPASS_REPO_MODULOS=${LHAVE}" >> envlocal.env
+
+                        """
+
+                    }
+
+                    sh """
+                    cd infra
+                    echo "" >> envlocal.env
+                    echo "export APP_MAIL_SERVIDOR=relay.nuvem.gov.br" >> envlocal.env
+                    echo "export KUBERNETES_RESOURCES_INFORMAR=false" >> envlocal.env
+                    echo "export APP_HOST=${JOB_URL}" >> envlocal.env
+                    echo "export APP_ORGAO=${JOB_ORGAO}" >> envlocal.env
+                    echo "export APP_FONTES_GIT_CHECKOUT=${GITSEIVERSAO}" >> envlocal.env
+                    echo "export KUBERNETES_NAMESPACE=${JOB_NS}" >> envlocal.env
+                    echo "export KUBERNETES_PVC_STORAGECLASS_ANEXOS=nfs-client" >> envlocal.env
+                    echo "export KUBERNETES_PVC_STORAGECLASS_DB=nfs-client" >> envlocal.env
+                    echo "export KUBERNETES_PVC_STORAGECLASS_FONTES=nfs-client" >> envlocal.env
+                    echo "export KUBERNETES_PVC_STORAGECLASS_SOLR=nfs-client" >> envlocal.env
+                    echo "export KUBERNETES_PVC_STORAGECLASS_CONTROLADORINST=nfs-client" >> envlocal.env
+                    echo "export KUBERNETES_LIMITS_MEMORY_SOLR=1.5Gi" >> envlocal.env
+                    echo "export KUBERNETES_LIMITS_CPU_SOLR=1000m" >> envlocal.env
+                    echo "export KUBERNETES_REQUEST_MEMORY_SOLR=1.5Gi" >> envlocal.env
+                    echo "export KUBERNETES_REQUEST_CPU_SOLR=1000m" >> envlocal.env
+                    echo "export KUBERNETES_LIMITS_MEMORY_DB=1Gi" >> envlocal.env
+                    echo "export KUBERNETES_LIMITS_CPU_DB=1000m" >> envlocal.env
+                    echo "export KUBERNETES_REQUEST_MEMORY_DB=1Gi" >> envlocal.env
+                    echo "export KUBERNETES_REQUEST_CPU_DB=1000m" >> envlocal.env
+                    echo "export KUBERNETES_LIMITS_MEMORY_APP=1Gi" >> envlocal.env
+                    echo "export KUBERNETES_LIMITS_CPU_APP=1000m" >> envlocal.env
+                    echo "export KUBERNETES_REQUEST_MEMORY_APP=1Gi" >> envlocal.env
+                    echo "export KUBERNETES_REQUEST_CPU_APP=1000m" >> envlocal.env
+                    """
+
+                    sh """
+
+                    cd infra
+
+                    echo "export MODULO_PI_INSTALAR=${MODULOPI_INSTALAR}" >> envlocal.env
+                    echo "export MODULO_PI_VERSAO=${MODULOPI_VERSAO}" >> envlocal.env
+                    echo "export MODULO_PI_URL=${MODULOPI_URL}" >> envlocal.env
+                    echo "export MODULO_PI_EMAIL=${MODULOPI_EMAIL}" >> envlocal.env
+
+                    """
+
+                    withCredentials([ string(credentialsId: MODULOPI_USUARIO, variable: 'LHAVE')]) {
+
+                        sh """
+
+                        cd infra
+                        echo "export MODULO_PI_USUARIO=${LHAVE}" >> envlocal.env
+
+                        """
+                    }
+
+                    withCredentials([ string(credentialsId: MODULOPI_SENHA, variable: 'LHAVE')]) {
+
+                        sh """
+
+                        cd infra
+                        echo "export MODULO_PI_SENHA=${LHAVE}" >> envlocal.env
+
+                        """
+                    }
+
+                    sh """
+
+                    cd infra
+
+                    make kubernetes_montar_yaml
+                    make kubernetes_delete || true
+
+                    make kubernetes_montar_yaml
+                    make kubernetes_apply
+
+                    sleep 20
+                    kubectl -n ${JOB_NS} scale --replicas=0 deployment/jod || true
+                    """
+
+
+                }
+
+            }
+        }
+
+        stage('Rodando Atualizador'){
+
+            steps {
+
+                dir('kube'){
+
+                    sh """
+
+                    sleep 60
+                    i=0
+                    while true; do
+
+                      echo "Vamos printar o log dessa execucao, atencao que pode haver mais logs acima de outras tentativas..."
+                      set +e
+                      kubectl -n ${JOB_NS} logs -f job.batch/sei-inicializador
+                      set -e
+
+                      if kubectl -n ${JOB_NS} wait --for=condition=complete job/sei-inicializador --timeout=0 2>/dev/null; then
+                        job_result=0
+                        break
+                      fi
+
+                      if kubectl -n ${JOB_NS} wait --for=condition=failed job/sei-inicializador --timeout=0 2>/dev/null; then
+                        job_result=1
+                        break
+                      fi
+
+                      sleep 10
+
+
+                    done
+
+                    if [[ \$job_result -eq 1 ]]; then
+                        echo "Job failed!"
+                        exit 1
+                    fi
+
+                    """
+
+                }
+            }
+        }
+
+        stage('Verificando Componentes Kube'){
+
+            parallel {
+
+                stage('Database'){
+                    steps {
+                        dir('kube'){
+                            sh """
+                            cd infra
+                            make KUBE_DEPLOY_NAME=db kubernetes_check_deploy_generic
+                            """
+                        }
+                    }
+                }
+
+
+                stage('JOD'){
+                    steps {
+                        dir('kube'){
+                            sh """
+                            cd infra
+                            make KUBE_DEPLOY_NAME=jod kubernetes_check_deploy_generic
+                            """
+                        }
+                    }
+                }
+
+
+                stage('Memcached'){
+                    steps {
+                        dir('kube'){
+                            sh """
+                            cd infra
+                            make KUBE_DEPLOY_NAME=memcached kubernetes_check_deploy_generic
+                            """
+                        }
+                    }
+                }
+
+
+                stage('Solr'){
+                    steps {
+                        dir('kube'){
+                            sh """
+                            cd infra
+                            make KUBE_DEPLOY_NAME=solr kubernetes_check_deploy_generic
+                            """
+                        }
+                    }
+                }
+
+
+                stage('APP'){
+                    steps {
+                        dir('kube'){
+                            sh """
+                            cd infra
+                            make kube_timeout=1200s KUBE_DEPLOY_NAME=sei-app kubernetes_check_deploy_generic
+                            """
+                        }
+                    }
+                }
+
+
+                stage('URL Respondendo'){
+                    steps {
+                        dir('kube'){
+                            sh """
+                            cd infra
+                            echo "export APP_HOST=${JOB_URL}" >> envlocal.env
+                            make check_isalive-timeout=1200 check-sei-isalive
+                            """
+                        }
+                    }
+                }
+
+
+
+            }
+
+        }
+
+    }
+
+}
+
