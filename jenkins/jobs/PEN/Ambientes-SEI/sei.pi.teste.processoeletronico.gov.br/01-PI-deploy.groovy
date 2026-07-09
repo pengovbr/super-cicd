e37869f89cd8137fe262616030df1f4715cc9e19
Wellington Palmeira <wellington.palmeira@gestao.gov.br>
2026-07-09 11:35:35 -0300

    Updates no ambiente do sei.pi

--- jenkins/jobs/PEN/Ambientes-SEI/sei.pi.teste.processoeletronico.gov.br/01-PI-deploy.groovy
+++ jenkins/jobs/PEN/Ambientes-SEI/sei.pi.teste.processoeletronico.gov.br/01-PI-deploy.groovy
@@ -262,7 +262,7 @@ pipeline {
                         kubectl -n ${JOB_NS} scale --replicas=0 deployment/jod || true
 
                         echo "Removendo diretório do módulo PI dentro do container do app para forçar atualização"
-                        POD_APP=\$(kubectl -n ${JOB_NS} get pods -l app=sei-app -o jsonpath='{.items[0].metadata.name}' 2>/dev/null || true)
+                        POD_APP=\$(kubectl -n ${JOB_NS} get pods -o name | grep -E 'pod/sei-app(-agendador)?-' | head -n 1 | sed 's#^pod/##' 2>/dev/null || true)
                         if [ -n "\${POD_APP}" ]; then
                             kubectl -n ${JOB_NS} exec "\${POD_APP}" -- sh -c 'rm -rf /opt/sei/web/modulos/protocolo-integrado; rm -f /sei/controlador-instalacoes/instalado-modulo-pi.ok' || true
                         fi
